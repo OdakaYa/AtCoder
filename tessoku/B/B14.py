327163aedb12d4a19a9f3ee89690563ce4cf3428
Odaka <odaka.y.ac@m.titech.ac.jp>
2024-03-14 01:19:50 +0900

    update

--- tessoku/B/B14.py
+++ tessoku/B/B14.py
@@ -1,14 +1,12 @@
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
-A.sort()
 
 dp = [False]*(K+1)
-
 dp[0] = True
-for i in range(N):
-    for j in range(K+1):
-        if dp[K-j] and (K-j)+A[i] <= K:
-            dp[(K-j)+A[i]] = True
+for a in A:
+    for j in reversed(range(K+1)):
+        if dp[j] and j+a <= K:
+            dp[j+a] = True
 
 if dp[K]:
     print("Yes")
