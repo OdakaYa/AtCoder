4ffad38b41497b7c648be52c1587fc6eaaee5338
Odaka <odaka.y.ac@m.titech.ac.jp>
2024-04-22 00:07:39 +0900

    update

--- tessoku/B/B69.py
+++ tessoku/B/B69.py
@@ -1,62 +1,60 @@
-INF = 1 << 50
-class Edge:
+class Edge():
     def __init__(self, to, cap, rev):
         self.to = to
         self.cap = cap
         self.rev = rev
 
-class MaxFlow:
+class MaxFlow():
     def __init__(self, N):
         self.size = N
         self.g = [[] for _ in range(N)]
     
-    def add_edge(self, a, b, c):
-        g = self.g
+    def add(self, a, b, c):
         e = Edge(b, c, None)
         rev = Edge(a, 0, e)
         e.rev = rev
-        g[a].append(e)
-        g[b].append(rev)
+        self.g[a].append(e)
+        self.g[b].append(rev)
     
-    def dfs(self, u, goal, flow):
+    def dfs(self, u, goal, flw):
         if u == goal:
-            return flow
+            return flw
         self.visited[u] = True
         for e in self.g[u]:
             v = e.to
             if self.visited[v] or e.cap == 0:
                 continue
-            f = self.dfs(v, goal, min(flow, e.cap))
-            if f:
-                e.cap -= f
-                e.rev.cap += f
-                return f
+            tmp = self.dfs(e.to, goal, min(flw, e.cap))
+            if tmp > 0:
+                e.cap -= tmp
+                e.rev.cap += tmp
+                return tmp
         return 0
-
-    def max_flow(self, s, goal):
+    
+    def maxflow(self, s, goal):
         res = 0
         while True:
             self.visited = [False]*self.size
-            net_flow = self.dfs(s, goal, INF)
-            if net_flow == 0:
+            nf = self.dfs(s, goal, 10**50)
+            if nf == 0:
                 break
-            res += net_flow
+            res += nf
         return res
 
 N, M = map(int, input().split())
 g = MaxFlow(N+26)
 for i in range(N):
     C = input()
-    for j, c in enumerate(C):
-        if c == "1":
-            g.add_edge(i+1, j+N+1, 1)
-    g.add_edge(0, i+1, 10)
+    g.add(0, i+1, 10)
+    for j in range(24):
+        if C[j] == "1":
+            g.add(i+1, j+N+1, 1)
 
 for j in range(24):
-    g.add_edge(j+N+1, N+25, M)
+    g.add(j+N+1, N+25, M)
 
-f = g.max_flow(0, N+25)
-if f >= M*24:
+res = g.maxflow(0, N+25)
+if res >= 24*M:
     print("Yes")
 else:
     print("No")
